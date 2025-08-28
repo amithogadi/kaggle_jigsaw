06a92e8706ec80b17da5e7ecc8ab37f1b3b677c5
Amit <amithogadi@gmail.com>
2025-08-28 09:02:07 +0530

    save load : Data -> comment

--- kaggle/utils/comment.py
+++ kaggle/utils/comment.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 @dataclass
-class Data:
+class Comment:
     row_id: int
     body: str
     rule: str
@@ -17,7 +17,14 @@ class Data:
     rule_violation: Optional[float] = None
 
     @staticmethod
-    def load(file_path: str) -> List['Data']:
+    def load(file_path: str) -> List['Comment']:
         df = pd.read_csv(file_path)
         dicts = df.to_dict(orient='records')
-        return [Data(**d) for d in dicts]
+        return [Comment(**d) for d in dicts]
+
+    @staticmethod
+    def save(file_path: str, data: List['Comment']) -> None:
+        df = pd.DataFrame([{'row_id': comment.row_id, 'rule_violation': comment.rule_violation}
+                          for comment in sorted(data, key=lambda x:x.row_id)])
+        df.to_csv(file_path, index=False)
+
